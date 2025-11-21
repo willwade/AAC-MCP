00ac673cca5674e71e920dfd40078c57fddae21a
will wade <willwade@gmail.com>
2025-11-21 19:02:40 +0000

    Add AAC processor catalog and lookup tool

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import { FastMCP } from "fastmcp";
 
 import explainCodePrompt from "./prompts/explain-code.js";
 import gitCommitPrompt from "./prompts/git-commit.js";
+import aacProcessorsResource from "./resources/aac-processors.js";
 import logsResource from "./resources/logs.js";
+import aacProcessorTool from "./tools/aac-processor.js";
 import sumTool from "./tools/sum.js";
 
 const server = new FastMCP({
@@ -15,9 +17,11 @@ server.addPrompt(explainCodePrompt);
 server.addPrompt(gitCommitPrompt);
 
 // resources
+server.addResource(aacProcessorsResource);
 server.addResource(logsResource);
 
 // tools
+server.addTool(aacProcessorTool);
 server.addTool(sumTool);
 
 server.start({
